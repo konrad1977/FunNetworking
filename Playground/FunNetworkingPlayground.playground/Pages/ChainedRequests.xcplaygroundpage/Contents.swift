e0b7cd612a26b776febd108a2ccc3ca4370025ec
Mikael Konradsson <mikael.konradsson@outlook.com>
2021-04-27 21:10:46 +0200

    Cleaned up examples a bit

--- Playground/FunNetworkingPlayground.playground/Pages/ChainedRequests.xcplaygroundpage/Contents.swift
+++ Playground/FunNetworkingPlayground.playground/Pages/ChainedRequests.xcplaygroundpage/Contents.swift
@@ -5,48 +5,44 @@ import FunNetworking
 import Funswift
 
 let requestWithCachePolicy = flip(curry(URLRequest.init(url:cachePolicy:timeoutInterval:)))
-let urlRequesstWithTimeout = flip(requestWithCachePolicy(.returnCacheDataElseLoad))
+let requestWithTimeout = flip(requestWithCachePolicy(.returnCacheDataElseLoad))
 
 let getIpNumberBase = "https://api.ipify.org/?format=json"
 let getIpInfoUrl: (Host) -> String = { host in "https://ipinfo.io/\(host.ip)/geo" }
 
+struct Host: Decodable { let ip: String }
 struct IpInfo: Decodable {
     let ip: String, city: String, region: String, country: String, loc: String, postal: String
 }
 
-struct Host: Decodable { let ip: String }
-
-extension String: Error {}
-
-let fetchIpNumber: () -> IO<Either<Error, Host>> = {
+let fetchIpNumber: IO<Either<Error, Host>> = {
 	getIpNumberBase
 		|> URL.init(string:)
-		>=> urlRequesstWithTimeout(30)
+		>=> requestWithTimeout(30)
 		|> syncRequest
 		<&> decodeJsonData |> logger
-}
+}()
 
-func fetchExtendedInformationFrom(host: Either<Error, Host>) -> IO<Either<Error, IpInfo>> {
-    switch host {
-    case let .right(host):
-        return getIpInfoUrl(host)
-            |> URL.init(string:)
-            >=> urlRequesstWithTimeout(30)
-            |> retry(syncRequest, retries: 3)
-            <&> decodeJsonData
-    case let .left(error):
-        return IO { .left(error) }
-    }
+let fetchHostInfo: (Either<Error, Host>) -> IO<Either<Error, IpInfo>> = { host in
+	switch host {
+	case let .right(host):
+		return getIpInfoUrl(host)
+			|> URL.init(string:)
+			>=> requestWithTimeout(30)
+			|> retry(syncRequest, retries: 3)
+			<&> decodeJsonData
+	case let .left(error):
+		return IO { .left(error) }
+	}
 }
-
 
 // Syntax alternative 1
-let ipInfoFetcher = fetchIpNumber() >>- fetchExtendedInformationFrom
+let ipInfoFetcher = fetchIpNumber >>- fetchHostInfo
 dump(ipInfoFetcher.unsafeRun())
 
 // Syntax alternative 2
-//fetchMyIpNumber()
-//    .flatMap(fetchExtendedInformationFrom)
+//fetchIpNumber
+//    .flatMap(fetchHostInfo)
 //    .unsafeRun()
 
 //: [Next](@next)
