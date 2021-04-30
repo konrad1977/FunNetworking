802f7916e9cb600a8666f6779cae0f33a8e25429
Mikael Konradsson <mikael.konradsson@bontouch.com>
2021-04-30 11:37:10 +0200

    Add linear cooldown - needs test

--- Sources/FunNetworking/Retryability/RetryResult.swift
+++ Sources/FunNetworking/Retryability/RetryResult.swift
@@ -10,26 +10,33 @@ import Funswift
 
 // MARK: - Retry on Result
 public func retry<A, B, E: Error>(
-	_ f: @escaping (A) -> Result<B, E>, retries: Int
+    _ f: @escaping (A) -> Result<B, E>,
+    retries: Int,
+    coolDown: TimeInterval
 ) -> (A) -> Result<B, E> {
-	retry(f)(retries)
+	retry(f)(coolDown)(retries)
 }
 
 public func retry<A, B, E: Error>(
 	_ f: @escaping (A) -> Result<B, E>
-) -> (Int) -> (A) -> Result<B, E> {
+) -> (TimeInterval) -> (Int) -> (A) -> Result<B, E> {
 
-	func retry(value: A, result: Result<B, E>, currentRun: Int) -> Result<B, E> {
+    func retry(value: A, result: Result<B, E>, currentRun: Int, cooldown: TimeInterval) -> Result<B, E> {
 		switch result {
 		case .success:
 			return result
 		case let .failure(error):
-			return currentRun > 0
-				? retry(value: value, result: f(value), currentRun: currentRun - 1)
-				: .failure(error)
+            if currentRun > 0 {
+                if cooldown > 0 {
+                    Thread.sleep(forTimeInterval: cooldown)
+                }
+                return retry(value: value, result: f(value), currentRun: currentRun - 1, cooldown: cooldown)
+            } else {
+                return .failure(error)
+            }
 		}
 	}
-	return { retries in { value in retry(value: value, result: f(value), currentRun: retries - 1) } }
+    return { cooldown in { retries in { value in retry(value: value, result: f(value), currentRun: retries - 1, cooldown: cooldown) } } }
 }
 
 
