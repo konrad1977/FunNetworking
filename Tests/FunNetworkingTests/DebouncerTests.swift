378b2c5f220cd7e28c424174fc577a9994fee69c
Mikael Konradsson <mikael.konradsson@bontouch.com>
2021-05-01 14:18:46 +0200

    Add a santity check for linear debounce

--- Tests/FunNetworkingTests/DebouncerTests.swift
+++ Tests/FunNetworkingTests/DebouncerTests.swift
@@ -6,12 +6,12 @@ final class DebouncerTests: XCTestCase {
     func testValueLinearOneRun() {
         let result = Debounce.linear(10).run()
         XCTAssertEqual(10, result.value)
+        XCTAssertEqual(10, result.run().value)
     }
 
     func testValueLinearExpOneRun() {
         let result = Debounce.exponential(10).run()
         XCTAssertEqual(20, result.value)
-
         XCTAssertEqual(40, result.run().value)
     }
 }
